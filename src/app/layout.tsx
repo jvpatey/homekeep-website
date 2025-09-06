d9119a2c6a7254d556b93d788842402a18e1f202
Jeffrey Patey <jeffreyvpatey@gmail.com>
2025-09-06 14:31:47 -0300

    fixed favicon error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,12 +22,14 @@ export const metadata: Metadata = {
   authors: [{ name: "HomeKeep" }],
   icons: {
     icon: [
+      { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
+    shortcut: "/favicon.ico",
   },
   openGraph: {
     title: "HomeKeep - Never forget home maintenance again!",
